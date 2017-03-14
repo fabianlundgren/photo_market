1e76583a17416de5d4117354988b049c8b2d57a3
fabianlundgren <fabbe.lundgren@gmail.com>
2017-03-14 10:47:51 +0100

    Add task to Rakefile and changed the coverall require

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,6 @@
-require 'cucumber/rails'
 require 'coveralls'
-Coveralls.wear!('rails')
+Coveralls.wear_merged!('rails')
+require 'cucumber/rails'
 
 ActionController::Base.allow_rescue = false
 begin
