164653baea98671acdc594645773dd7f15b478f1
casperjureskog <lcasper@mac.com>
2017-03-14 20:22:29 +0100

    removes un used files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314103548) do
+ActiveRecord::Schema.define(version: 20170314132242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
