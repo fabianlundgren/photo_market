1223565a0f07c4b95224cc1117c892c051ecb884
johan <johan.schantz@telia.com>
2017-03-15 11:15:06 +0100

    added first rspec for Shopping cart

--- app/controllers/shopping_cart_controller.rb
+++ app/controllers/shopping_cart_controller.rb
@@ -1,8 +1,23 @@
 class ShoppingCartController < ApplicationController
+
   def add_to_cart
+    # If there is a cart stored in cookies, use that cart
+    # Otherwise, create a new one.
+    if cookies['cart_id']
+      #add to that cart
+    else
+      # create new cart
+      # store cart in cookies
+      # https://github.com/AmberWilkie/sf-online-august/blob/master/app/controllers/carts_controller.rb
+      # session[:cart_id] = @cart.id
+      # https://github.com/AmberWilkie/sf-online-august/blob/master/features/support/hooks.rb
+    end
+    @cart = Cart.create
+
     # create a shopping cart or check for one that already exists (cookies)
+    @image = Image.find(params[:id])
     # find the image based on its id (from params)
+    @cart.add(@image, @image.price)
     # add that image to the shopping cart
-    # https://github.com/crowdint/acts_as_shopping_cart
   end
 end
--- spec/models/shopping_cart_spec.rb
+++ spec/models/shopping_cart_spec.rb
@@ -2,11 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ShoppingCart, type: :model do
   describe 'DB table' do
-
-  end
-
-  describe 'validations' do
-
+    it { is_expected.to have_db_column(:id).of_type(:integer)}
   end
 
   describe 'associations' do
